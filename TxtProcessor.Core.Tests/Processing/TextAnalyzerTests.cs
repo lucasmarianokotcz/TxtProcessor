7b9f900ca3a583928cd759b6271410616cb22298
lucasmarianokotcz <lucasmariano-@hotmail.com>
2025-12-20 23:00:36 -0300

    styles de código e projeto teste funcionando para o TextAnalyzer

---
+++ TxtProcessor.Core.Tests/Processing/TextAnalyzerTests.cs
@@ -0,0 +1,23 @@
+﻿using TxtProcessor.Core.Processing;
+
+namespace TxtProcessor.Core.Tests.Processing;
+
+public class TextAnalyzerTests
+{
+    [Fact]
+    public void GetTopWords_Returns_Most_Repeated_Words()
+    {
+        // Arrange
+        var text = "apple banana apple orange banana apple";
+
+        // Act
+        var result = TextAnalyzer.GetTopWords(text);
+
+        // Assert
+        Assert.Equal(3, result.Count);
+
+        Assert.Equal(("apple", 3), result[0]);
+        Assert.Equal(("banana", 2), result[1]);
+        Assert.Equal(("orange", 1), result[2]);
+    }
+}
--- TxtProcessor.Core/Processing/TextAnalyzer.cs
+++ TxtProcessor.Core/Processing/TextAnalyzer.cs
@@ -1,26 +1,25 @@
-﻿namespace TxtProcessor.Core.Processing
+﻿namespace TxtProcessor.Core.Processing;
+
+public static class TextAnalyzer
 {
-    public static class TextAnalyzer
+    public static IReadOnlyList<(string Word, int Count)> GetTopWords(
+        string text,
+        int top = 5)
     {
-        public static IReadOnlyList<(string Word, int Count)> GetTopWords(
-            string text,
-            int top = 5)
-        {
-            if (string.IsNullOrWhiteSpace(text))
-                return [];
+        if (string.IsNullOrWhiteSpace(text))
+            return [];
 
-            var words = text
-                .ToLowerInvariant()
-                .Split(
-                    separator: [' ', '\r', '\n', '\t', '.', ',', ';', '!', '?'],
-                    options: StringSplitOptions.RemoveEmptyEntries);
+        var words = text
+            .ToLowerInvariant()
+            .Split(
+                separator: [' ', '\r', '\n', '\t', '.', ',', ';', '!', '?'],
+                options: StringSplitOptions.RemoveEmptyEntries);
 
-            return [.. words
-                .GroupBy(w => w)
-                .Select(g => (Word: g.Key, Count: g.Count()))
-                .OrderByDescending(x => x.Count)
-                .ThenBy(x => x.Word)
-                .Take(top)];
-        }
+        return [.. words
+            .GroupBy(w => w)
+            .Select(g => (Word: g.Key, Count: g.Count()))
+            .OrderByDescending(x => x.Count)
+            .ThenBy(x => x.Word)
+            .Take(top)];
     }
 }
