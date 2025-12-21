2694310a64e7757991c2eafb7afa9c6abad30417
lucasmarianokotcz <lucasmariano-@hotmail.com>
2025-12-20 22:51:37 -0300

    criação do analisador de texto

---
+++ TxtProcessor.Core/Processing/TextAnalyzer.cs
@@ -0,0 +1,26 @@
+﻿namespace TxtProcessor.Core.Processing
+{
+    public static class TextAnalyzer
+    {
+        public static IReadOnlyList<(string Word, int Count)> GetTopWords(
+            string text,
+            int top = 5)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return [];
+
+            var words = text
+                .ToLowerInvariant()
+                .Split(
+                    separator: [' ', '\r', '\n', '\t', '.', ',', ';', '!', '?'],
+                    options: StringSplitOptions.RemoveEmptyEntries);
+
+            return [.. words
+                .GroupBy(w => w)
+                .Select(g => (Word: g.Key, Count: g.Count()))
+                .OrderByDescending(x => x.Count)
+                .ThenBy(x => x.Word)
+                .Take(top)];
+        }
+    }
+}
